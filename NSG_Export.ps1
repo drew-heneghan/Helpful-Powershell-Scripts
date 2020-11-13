54af8dd6e84e323bdc864514c739c74b3e7a98c8
drewseph1 <67722846+drewseph1@users.noreply.github.com>
2020-11-12 20:17:44 -0600

    NSG, File Export and IP Checker

---
+++ NSG_Export.ps1
@@ -0,0 +1,104 @@
+﻿#--------Global Variables-------------------- 
+
+$reportFileName = "XXXXXXX.csv" 
+
+$report = @() 
+
+$total_subnets = @() 
+
+$total_NICs = @() 
+
+$nsgs = Get-AzNetworkSecurityGroup 
+
+#--------------------- 
+
+  
+
+#--------Main---------- 
+
+foreach($nsg in $nsgs) 
+{ 
+    #--------Local Variables------------ 
+    $subnetsInNSG = $nsg.SubnetsText 
+
+    $NICSInNSG = $nsg.NetworkInterfacesText 
+    #-----------------------------------   
+    #Gather subnet and NIC data first.  Code is the same for both.  Will look to generalize into function later. 
+
+    if($subnetsInNSG -ne $null) 
+    {   
+        #The nsg.SubnetsText comes in JSON form.  Convert it to an array so I can change it for csv output 
+        $subnetJSON = ConvertFrom-Json –InputObject $subnetsInNSG 
+
+        <#When pulling from Azure, the subnet/nic data is in a key, value pair
+        #ID Value == Data we want
+
+        foreach($subnet in $subnetJSON.Id) 
+
+        {   #slice up string slash delimited to remove them from output 
+            $subnet = $subnet.Split("/") 
+            #subnet[8] = the VNET.  Here i'm adding the subnet name along with the VNET it's in between ( ) 
+            $subnet_to_add = $subnet[$subnet.Length - 1] + "(" + $subnet[8] + ")" 
+            #The last piece in the string is the name of the data we want, so add only that to array 
+            $total_subnets+=$subnet_to_add 
+       } 
+       #Join all elements again, separated by comma so it's easy to read/sort through for any potential analysis 
+       $total_subnets = $total_subnets -join ',' 
+    } 
+
+     
+
+    if($NICSInNSG -ne $null) 
+    { 
+        $NICJSON = ConvertFrom-Json –InputObject $NICSInNSG 
+ 
+        foreach($nic in $NICJSON.Id) 
+        {               
+            $nic = $nic.Split("/") 
+            $nic_data = Get-AzNetworkInterface -Name $nic[$nic.Length - 1] | Select-Object -Property IpConfigurationsText        
+            $nic_data = ConvertFrom-Json -InputObject $nic_data.IpConfigurationsText
+
+            foreach($temp_nic in $nic_data.Subnet.Id)
+            {
+               #Pull in same manner as above, except we're adding VNETs to csv as well.
+               #$temp_nic[$temp_nic.Length - 3] = VNET
+               #$temp_nic[$temp_nic.Length - 1] = SUBNET
+               $temp_nic = $temp_nic.split("/")           
+               $nic_to_add = $nic[$nic.Length - 1] + "(" + $temp_nic[$temp_nic.Length - 3] + "\" + $temp_nic[$temp_nic.Length - 1] + ")"
+               $total_NICs+=$nic_to_add
+            }
+       } 
+       $total_NICs = $total_NICs -join ',' 
+    } 
+
+    #Pull default rules using the config call.Add NSG name, subnet and NICs for organization and append to the overall report 
+
+    $def_rules = Get-AzNetworkSecurityRuleConfig -DefaultRules -NetworkSecurityGroup $nsg | Select-Object -Property @{n="NSG Name";e={$nsg.Name}},@{n="Rule Name";e={$_.Name}},@{n="Subnets";e={$total_subnets}}, @{n="NICs";e={$total_NICs}}, Direction, Priority,
+    @{n="SourcePortRange";e={$_.SourcePortRange}}, 
+    @{n="DestinationPortRange";e={$_.DestinationPortRange}}, 
+    @{n="SourceAddressPrefix";e={$_.SourceAddressPrefix}}, 
+    @{n="DestinationAddressPrefix";e={$_.DestinationAddressPrefix}},
+    Protocol,Access,Description 
+
+    $report+=$def_rules 
+
+    #same as above but for user created rules.   
+
+    $sec_rules = Get-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg | Select-Object -Property @{n="NSG Name";e={$nsg.Name}},@{n="Rule Name";e={$_.Name}},@{n="Subnets";e={$total_subnets}}, @{n="NICs";e={$total_NICs}}, Direction, Priority,
+    @{n="SourcePortRange";e={$_.SourcePortRange}}, 
+    @{n="DestinationPortRange";e={$_.DestinationPortRange}}, 
+    @{n="SourceAddressPrefix";e={$_.SourceAddressPrefix}}, 
+    @{n="DestinationAddressPrefix";e={$_.DestinationAddressPrefix}},
+    Protocol,Access,Description 
+
+    $report+=$sec_rules 
+
+    Write-Host("NSG: $($nsg.Name)") 
+
+    #Empty subnet/nic arrays 
+
+    $total_subnets = @() 
+    $total_NICs = @() 
+} 
+
+$report | Export-CSV "~\$($reportFileName)"-NoTypeInformation 
---
+++ Unzip_&_Extension_Chng.ps1
@@ -0,0 +1,57 @@
+﻿#Script to change all .ear, .war, and .zips to normal folders for Synopsis Detect scans
+#--------Variable definitions-------------
+$path = "C:\path\to\directory\" 
+$full_path = $path+"*" #Star is required for recursion, must append to end of path above
+#----------Function Definitions----------
+Function Unzip {
+[CmdletBinding()]
+Param([Parameter(ValueFromPIpeline)]
+$fileInfo
+)
+    #Write-Host("Name:$($fileInfo.FullName)")
+    Write-Host("Extracting $($fileInfo.FullName) to $($fileInfo.Directory)")
+    Set-Location -Path $fileInfo.Directory #Same thing as cd XXXX.  Had to in order for expanded .zips to go to zip location
+    Expand-Archive -Path $fileInfo.FullName #Open the zip and place it's contents
+    if(Test-Path $fileInfo.FullName -PathType Leaf)
+    {
+        Write-Host("Extraction successful.  Removing $($fileInfo.Name)")
+        Remove-Item -Path $fileInfo.FullName #Delete the zip
+    }
+    else
+    {
+        Write-Host("File was not extracted correctly")
+    }
+} 
+
+Function convertToZip{
+[CmdletBinding()]
+Param([Parameter(ValueFromPIpeline)]
+$fileInfo
+)
+Write-Host("Converting: $($fileInfo.Name) to .zip")
+Set-Location -Path $fileInfo.Directory
+Rename-Item -Path $fileInfo.FullName -NewName $([System.io.path]::ChangeExtension($fileInfo.Name,".zip"))
+}
+
+#--------Main--------------
+#Placed in a Do While loop so it reruns and dives 
+Do {
+Get-ChildItem -Path $full_path -Include *.zip,*ear,*war -Recurse | 
+%{
+    if($_.Extension -eq ".zip")
+    {
+        $_ | Unzip
+    }
+    elseif($_.Extension -eq ".ear" -or $_.Extension -eq ".war")
+    {
+        $_ | convertToZip
+
+    }
+}
+
+} while(Get-ChildItem -Path $full_path -Include *zip, *ear, *war -Recurse)
+
+
+
+
+
